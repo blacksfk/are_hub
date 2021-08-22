1cf9422b55e146f8858657efba5f8b231b8e1395
blacksfk <blacksunflowakuttaspam@hotmail.com>
2021-08-22 19:18:47 +1000

    All() now returns users without passwords and keys

--- mongodb/user.go
+++ mongodb/user.go
@@ -4,7 +4,9 @@ import (
 	"context"
 
 	"github.com/blacksfk/are_hub"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Implements are_hub.UserRepo.
@@ -17,10 +19,20 @@ func NewUserCollection(client *mongo.Client, db string) User {
 	return User{collection{client, db, "users"}}
 }
 
+// Get all users without their respective password and key.
 func (u User) All(ctx context.Context) ([]are_hub.User, error) {
 	var users []are_hub.User
 
-	return users, u.all(ctx, &users)
+	// hide the password and key
+	projection := bson.M{"password": 0, "key": 0}
+	cursor, e := u.get().Find(
+		ctx, bson.M{}, options.Find().SetProjection(projection))
+
+	if e != nil {
+		return nil, e
+	}
+
+	return users, cursor.All(ctx, &users)
 }
 
 func (u User) FindID(ctx context.Context, id string) (*are_hub.User, error) {
